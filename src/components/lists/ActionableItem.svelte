119e7b63561b4d8b6a988c56fd1c2829039553d8
Joan Andrés <andres6936@live.com>
2021-02-12 16:41:46 -0500

    Use of SVG icons

--- src/components/lists/ActionableItem.svelte
+++ src/components/lists/ActionableItem.svelte
@@ -1,20 +1,10 @@
 <script>
+    import IconBook from "../../svg/IconBook.svelte";
     // @type {string} The label of item.
     export let label;
-    // @type {number} Add badges if this parameter is greater that 0 to any
-    // group item to show unread counts, activity, and more.
-    export let badge = 0;
-    // @type {string} Define the id of collapsable element to show or hidden.
-    export let target = '';
 </script>
 
-{#if badge === 0}
-    <li class="list-group-item list-group-item-action" data-bs-toggle="collapse" data-bs-target="#{target}"
-        aria-expanded="false" aria-controls="Collapsable element">{label}</li>
-{:else }
-    <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
-        data-bs-toggle="collapse" data-bs-target="#{target}" aria-expanded="false" aria-controls="Collapsable element">
-        {label}
-        <span class="badge bg-primary rounded-pill">{badge}</span>
-    </li>
-{/if}
+<li class="list-group-item list-group-item-action">
+    <IconBook/>
+    {label}
+</li>
