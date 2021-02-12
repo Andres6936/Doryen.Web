28cfe310028bff5ac5dcf2ccac01cf6220b0e979
Joan Andrés <andres6936@live.com>
2021-02-12 11:20:22 -0500

    Fixed problem with Nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,7 +4,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">Documentation</a>
+        <a class="navbar-brand" href="index">Documentation</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#dr-navigation"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -12,7 +12,7 @@
         <div class="collapse navbar-collapse" id="dr-navigation">
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" aria-current="{segment === undefined ? 'page' : undefined}"
-                                        href="/">home</a></li>
+                                        href="index">home</a></li>
                 <li class="nav-item"><a class="nav-link" aria-current="{segment === 'about' ? 'page' : undefined}"
                                         href="about">about</a></li>
 
