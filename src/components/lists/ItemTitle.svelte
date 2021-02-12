e6d16e8de60d009731771ec53c45aeb210d58741
Joan Andrés <andres6936@live.com>
2021-02-12 10:31:47 -0500

    Use of ItemTitle element

---
+++ src/components/lists/ItemTitle.svelte
@@ -0,0 +1,6 @@
+<script>
+    // @type {string} The title of item.
+    export let title;
+</script>
+
+<li class="list-group-item list-group-item-dark">{title}</li>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,19 @@
 <script>
     import ListGroup from "../components/lists/ListGroup.svelte";
     import ActionableItem from "../components/lists/ActionableItem.svelte";
+    import ItemTitle from "../components/lists/ItemTitle.svelte";
 </script>
 <svelte:head>
     <title>Doryen Documentation</title>
 </svelte:head>
 
 <ListGroup>
-    <li class="list-group-item list-group-item-dark">Core</li>
+    <ItemTitle title="Core"/>
     <ActionableItem label="Colors"/>
     <ActionableItem label="Console" badge="5"/>
     <ActionableItem label="System layer" badge="6"/>
-    <li class="list-group-item list-group-item-dark">Base toolkit</li>
+
+    <ItemTitle title="Base toolkit"/>
     <ActionableItem label="Line drawing toolkit"/>
     <ActionableItem label="Image toolkit"/>
     <ActionableItem label="All purpose container"/>
@@ -20,7 +22,8 @@
     <ActionableItem label="Noise generator"/>
     <ActionableItem label="File parser"/>
     <ActionableItem label="Compression toolkit"/>
-    <li class="list-group-item list-group-item-dark">Roguelike toolkit</li>
+
+    <ItemTitle title="Roguelike toolkit"/>
     <ActionableItem label="BSP toolkit"/>
     <ActionableItem label="Field of view"/>
     <ActionableItem label="Heightmap toolkit"/>
