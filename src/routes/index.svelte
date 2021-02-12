a19748aadf527bcf594173ff531051fcb865ae67
Joan Andrés <andres6936@live.com>
2021-02-12 10:02:24 -0500

    Added tittle to list

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,9 +3,11 @@
 </svelte:head>
 
 <ul class="list-group">
+    <li class="list-group-item list-group-item-dark">Core</li>
     <li class="list-group-item list-group-item-action">Colors</li>
     <li class="list-group-item list-group-item-action">Console</li>
     <li class="list-group-item list-group-item-action">System layer</li>
+    <li class="list-group-item list-group-item-dark">Base toolkit</li>
     <li class="list-group-item list-group-item-action">Line drawing toolkit</li>
     <li class="list-group-item list-group-item-action">Image toolkit</li>
     <li class="list-group-item list-group-item-action">All purpose container</li>
@@ -14,6 +16,7 @@
     <li class="list-group-item list-group-item-action">Noise generator</li>
     <li class="list-group-item list-group-item-action">File parser</li>
     <li class="list-group-item list-group-item-action">Compression toolkit</li>
+    <li class="list-group-item list-group-item-dark">Roguelike toolkit</li>
     <li class="list-group-item list-group-item-action">BSP toolkit</li>
     <li class="list-group-item list-group-item-action">Field of view</li>
     <li class="list-group-item list-group-item-action">Heightmap toolkit</li>
