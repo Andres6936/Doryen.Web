0825c359acf2b4f12cee7869313160b650d8506a
Joan Andrés <andres6936@live.com>
2021-02-12 19:38:20 -0500

    Added documentation

--- src/server.js
+++ src/server.js
@@ -8,6 +8,12 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
     .use(
+        // This line is very important for the functioning of site, define the
+        // base path in the which the resources will be acquired, the purpose
+        // of this website is show the content in Github Pages, but in order to
+        // deliver files to the server it is necessary to specify the base path
+        // pointing to the project name, in case the base path is not present,
+        // Github pages will not render anything, showing a 'not found' message.
         dev === true ? '/' : 'Doryen.Web',
         compression({threshold: 0}),
         sirv('static', {dev}),
